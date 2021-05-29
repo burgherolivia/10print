be205970723bfa9c420a8ea0786f19398c4f4027
burghero <burgherolivia@gmail.com>
2021-05-29 14:15:01 -0500

    final 10print w/ animation!

--- liv.10print.js
+++ liv.10print.js
@@ -1,11 +1,19 @@
 
+const RESET = '\x1b[0m'
 
-const w = process.stdout.columns
+const sw = process.stdout.columns
 const char = ['⤧', '★', '♡', '▉', '≛', '☽', '▒']
 const color = ['\x1b[30m', '\x1b[37m', '\x1b[33m']
 
+let w = 0
+let d = 1
+
 function draw () {
   setTimeout(draw, 1000 / 12)
+  if (w > sw - 1 || w < 0) {
+    d = -d
+  }
+  w += d
   let output = '\x1b[45m'
   for (let i = 0; i < w; i++) {
     let r = Math.floor(Math.random() * char.length)
