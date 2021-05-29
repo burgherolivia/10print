25db08f288cfa3f4e5823d602e92325de6129adf
burghero <burgherolivia@gmail.com>
2021-05-29 13:43:32 -0500

    new pattern

--- liv.10print.js
+++ liv.10print.js
@@ -1,16 +1,16 @@
 
 
 const w = process.stdout.columns
+const char = ['⤧', '★', '♡', '▉', '≛', '☽', '▒']
+const color = ['\x1b[30m', '\x1b[37m', '\x1b[33m']
 
 function draw () {
-  setTimeout(draw, 1000)
+  setTimeout(draw, 1000 / 12)
   let output = '\x1b[45m'
   for (let i = 0; i < w; i++) {
-    if (Math.random() > 0.5) {
-      output += '\\'
-    } else {
-      output += '/'
-    }
+    let r = Math.floor(Math.random() * char.length)
+    let c = Math.floor(Math.random() * color.length)
+    output += color[c] + char[r]
   }
   console.log(output)
 }
